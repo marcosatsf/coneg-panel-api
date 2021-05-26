3cb1e78a1cd37993fdefe1d6b7e325754b66f262
MarcosATSF <marcos_atsf@hotmail.com>
2021-05-25 23:18:21 -0300

    Login test

--- app_coneg.py
+++ app_coneg.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.param_functions import Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from fastapi.middleware.cors import CORSMiddleware
 from db_transactions import PsqlPy
 import pandas as pd
@@ -7,7 +11,12 @@ import uvicorn
 import zipfile
 import os
 
+SECRET = os.urandom(24).hex()
+
 app = FastAPI()
+
+manager = LoginManager(SECRET, token_url='/auth/token')
+fake_db = {'marquin@gmail.com':{'password':'pass_test'}}
 
 origins = [
     "http://localhost",
@@ -46,7 +55,24 @@ def upload_file(
     return {
         "filename": file_rec.filename
     }
+
+# -------------------Login login---------------------
+@manager.user_loader
+def load_user(email: str):
+    user = fake_db.get(email)
+    return user
+
+@app.post('/auth/token')
+def login(data: OAuth2PasswordRequestForm = Depends()):
+    email = data.username
+    password = data.password
+
+    user = load_user(email)
+    if not user or password != user['password']:
+        raise InvalidCredentialsException
     
+    access_token = manager.create_access_token(data=dict(sub=email))
+    return {'access_token': access_token, 'token_type':'bearer'}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=5000, host='0.0.0.0')
